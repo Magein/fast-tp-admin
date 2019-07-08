9f284c133f892df5c0bc79151a9f9758d4f61da3
xiaomage <1154699897@qq.com>
2019-07-08 09:23:43 +0800

    update

--- render/Cdn.php
+++ render/Cdn.php
@@ -3,11 +3,16 @@
 namespace magein\render\admin;
 
 
+use think\Config;
+
 trait Cdn
 {
     protected function cdn()
     {
-        return [
+        // 配置文件中的
+        $resource = Config::get('resource') ?: [];
+
+        return array_merge([
             // 不稳定的CDN
             'layui_css' => 'https://cdn.90so.net/layui/2.4.5/css/layui.css',
             'layui_all_js' => 'https://cdn.90so.net/layui/2.4.5/layui.all.js',
@@ -19,6 +24,6 @@ trait Cdn
             'jquery_ztree_js' => 'https://cdn.staticfile.org/zTree.v3/3.5.40/js/jquery.ztree.all.min',
             'jquery_ztree_css' => 'https://cdn.staticfile.org/zTree.v3/3.5.40/css/zTreeStyle/zTreeStyle.css',
             'jquery_migrate' => 'https://cdn.staticfile.org/jquery-migrate/1.3.0/jquery-migrate.min',
-        ];
+        ], $resource);
     }
 }
