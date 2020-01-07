0bc020a2c2a5ec0643e387e037da265b551b6093
xiaomage <1154699897@qq.com>
2020-01-07 16:58:47 +0800

    update

--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -883,6 +883,27 @@ $(function () {
  * 2. 回填搜索框的值，搜索框的值从 url中获取
  */
 let province_id = parseInt($("[name=province_id]").val());
+if (isNaN(province_id)) {
+    province_id = parseInt($("[name=province_id]").data('value'));
+}
+getProvince(province_id);
+
+let city_id = parseInt($("[name=city_id]").val());
+if (isNaN(city_id)) {
+    city_id = parseInt($("[name=city_id]").data('value'));
+}
+if (city_id) {
+    changeCity(province_id, city_id);
+}
+
+let area_id = parseInt($("[name=area_id]").val());
+if (isNaN(area_id)) {
+    area_id = parseInt($("[name=area_id]").data('value'));
+}
+if (area_id) {
+    changeArea(province_id, city_id, area_id);
+}
+
 window.form.on('select(province_id)', function (data) {
     province_id = data.value;
     changeCity(province_id);
@@ -890,8 +911,6 @@ window.form.on('select(province_id)', function (data) {
 window.form.on('select(city_id)', function (data) {
     changeArea(province_id, data.value);
 });
-
-getProvince(province_id);
 
 /**
  * 获取省份数据
